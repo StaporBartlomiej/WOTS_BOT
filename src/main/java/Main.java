50912eb87a2207d9d9acfc141987741ad59c9bba
bstapor <bartlomiej.stapor@gmail.com>
2021-01-14 18:45:14 +0100

    Changed to reading img files and reading text from them and placing this into separate paragraphs in word document

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,112 @@
 import org.apache.commons.collections.CollectionUtils;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 public class Main {
 
     public static void main(String[] args) throws AWTException, InterruptedException {
-        Robot robot = new Robot();
-        for (int j = 0; j < 10000; j++) {
-            restAndStartTraining(robot);
-            for (int i = 0; i < 1000; i++) {
-                BufferedImage screenshot = JNAScreenShot.getScreenshot(new Rectangle(1350, 420, 550, 140));
-                String imgText = ScreenshotTextReader.getImgText(screenshot);
-                String last = getLastString(imgText);
-                System.out.println(last);
-                if (last.contains("wrong throwin") || last.contains("aren't training")) {
-                    break;
+        File folder = null;
+        try {
+            
+//            folder = ImageIO.read(new File("C:/Users/bartl/Documents/angelika/Leczenie_żywieniowe_Pierzak/"));
+            XWPFDocument document = new XWPFDocument();
+            FileOutputStream out = new FileOutputStream("test.docx");
+            folder = new File("C:/Users/bartl/Documents/angelika/Leczenie_żywieniowe_Pierzak/");
+            File[] listOfFiles = folder.listFiles();
+            if (listOfFiles != null) {
+                for (File file: listOfFiles) {
+                    BufferedImage image = ImageIO.read(file);
+                    String imgText = ScreenshotTextReader.getImgText(image);
+                    XWPFParagraph paragraph = document.createParagraph();
+                    XWPFRun run = paragraph.createRun();
+                    run.setText(imgText);
                 }
-
-                performAction(robot, last);
-                Thread.sleep(700);
             }
+            document.write(out);
+            out.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Robot robot = new Robot();
+//        for (int j = 0; j < 10000; j++) {
+//            restAndStartTraining(robot);
+//            for (int i = 0; i < 1000; i++) {
+//                BufferedImage screenshot = JNAScreenShot.getScreenshot(new Rectangle(1350, 420, 550, 140));
+//                String imgText = ScreenshotTextReader.getImgText(screenshot);
+//                String last = getLastString(imgText);
+//                System.out.println(last);
+//                performAction(robot, last);
+//                if (imgText.contains("initialized log throwing") || imgText.contains("os for these are: Low Throw")) {
+//                    performRandomThrow(robot);
+//                    Thread.sleep(700);
+//                    continue;
+//                }
+//                if (last.contains("aren't training")) {
+//                    performRandomThrow(robot);
+//                    break;
+//                }
+//                if (last.contains("don't need to rest'")) {
+//                    performRandomThrow(robot);
+//                    continue;
+//                }
+//                if (last.isEmpty()) {
+//                    performRandomThrow(robot);
+//                    continue;
+//                }
+//                if (imgText.contains("training has ended. Please wait 60 se")) {
+//                    Thread.sleep(60000);
+//                }
+////                if (last.contains("don't need to rest")) {
+////                    continue;
+////                }
+
+//                Thread.sleep(700);
+//            }
+//        }
+
+    }
+
+    private static void performRandomThrow(Robot robot) throws InterruptedException {
+        int nextThrow = getRandomNextThrow();
+        performRandomAction(robot, nextThrow);
+        Thread.sleep(700);
+    }
+
+    private static void performRandomAction(Robot robot, int nextThrow) {
+        if (nextThrow == 1) {
+            robot.keyPress(KeyEvent.VK_1); // 1
+            robot.keyRelease(KeyEvent.VK_1); // 1
+        } else if (nextThrow == 2) {
+            robot.keyPress(KeyEvent.VK_2); // 2
+            robot.keyRelease(KeyEvent.VK_2); // 2
+        } else if (nextThrow == 3) {
+            robot.keyPress(KeyEvent.VK_3); // 3
+            robot.keyRelease(KeyEvent.VK_3); // 3
         }
 
+    }
+
+    private static int getRandomNextThrow() {
+        Random random = new Random();
+        int next = random.nextInt(3);
+        if (next == 0) {
+            return getRandomNextThrow();
+        }
+        return next;
     }
 
     private static void restAndStartTraining(Robot robot) throws InterruptedException {
@@ -50,7 +130,8 @@ public class Main {
     private static String getLastString(String imgText) {
         String[] split = imgText.split("\n");
         List<String> next_throw = Stream.of(split)
-                .filter(s -> s.contains("next throw") || s.contains("aren't training"))
+                .filter(s -> s.contains("next throw") || s.contains("aren't training")
+                || s.contains("don't need to rest"))
                 .collect(Collectors.toList());
         if (CollectionUtils.isNotEmpty(next_throw)) {
             return next_throw.get(next_throw.size() - 1);
